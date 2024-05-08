b15502df9603d6d91697f7103b21d9846d26415e
Priyanshu Anand <anandpriyanshu12@gmail.com>
2024-05-08 14:55:35 +0530

    Fixed the issue

--- src/components/Newscontainer.jsx
+++ src/components/Newscontainer.jsx
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import Cards from './Cards'
 
+
 const Newscontainer = ({ category }) => {
+
 
     const [article, setarticle] = useState([])
 
-
-
     const infinitescroll = () => {
         if (window.innerHeight + window.scrollY + 200 >= document.body.offsetHeight) {
-            fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=aea4baffd3b141d3ac1509aee3c47fdd`)
+            fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=a294c971e5d541b896391a46a0594d56`)
                 .then((res) => res.json()).then((data) => { setarticle([...article, ...data.articles]) })
 
 
@@ -27,13 +27,14 @@ const Newscontainer = ({ category }) => {
 
 
     useEffect(() => {
-        fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=aea4baffd3b141d3ac1509aee3c47fdd`)
+        fetch(`https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=a294c971e5d541b896391a46a0594d56`)
             .then((res) => res.json()).then((data) => { setarticle(data.articles) })
 
 
     }, [category]);
     return (
         <>
+
             <div className="flex flex-col items-center justify-center ">
 
                 <div className='grid grid-cols-3 item-center gap-5'>
@@ -46,6 +47,8 @@ const Newscontainer = ({ category }) => {
                     }
                 </div>
             </div>
+
+
 
 
         </>
